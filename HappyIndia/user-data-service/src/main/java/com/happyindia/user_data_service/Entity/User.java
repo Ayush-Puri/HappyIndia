5b657b2b39840cdc43569cdaeb58682073ecb680
Ayush Puri <77268951+Ayush-Puri@users.noreply.github.com>
2024-12-24 23:13:31 +0530

    Added Service-registry, User-Service, Post-Service

---
+++ HappyIndia/user-data-service/src/main/java/com/happyindia/user_data_service/Entity/User.java
@@ -0,0 +1,11 @@
+
+
+
+public class User{
+    private int id;
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String Password;
+    private String Phone;
+}
---
+++ HappyIndia/user-data-service/src/test/java/com/happyindia/user_data_service/UserDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyindia.user_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
